f7be288b75b25b4ec569cfb7211a770e5cea39ac
Aniket Maurya <theaniketmaurya@gmail.com>
2024-01-18 18:14:06 +0000

    add navbar

--- image-gen/src/App.jsx
+++ image-gen/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <div className="flex items-center justify-center h-screen">
       <Card/>
     </div>
