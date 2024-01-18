f13354e9127163f3ebdd3e99d2a33a294346f962
Aniket Maurya <theaniketmaurya@gmail.com>
2024-01-18 19:13:27 +0000

    rename

--- image-gen/src/components/Navbar.jsx
+++ image-gen/src/components/Navbar.jsx
@@ -135,7 +135,7 @@ export default function Navbar() {
                     type="button"
                     className="w-full rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                   >
-                    Sign In
+                    Register
                   </button>
                   <button
                     type="button"
